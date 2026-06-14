581a49ead07a7e93c291c0b0d8074b8c18bfce67
David Lanz <dlanz_ve@yahoo.com>
2026-06-13 22:57:05 -0400

    modified:   C++/Ejercicio1_CMasMas.cpp 	modified:   C++/Ejercicio1_CMasMas.cpp

--- C++/Ejercicio1_CMasMas.cpp
+++ C++/Ejercicio1_CMasMas.cpp
@@ -1,8 +1,16 @@
-extern "C" int printf(const char* format, ...);
-
+// =============================================================================
+// Ejercicio1: Hola Mundo
+// AUTOR: David Lanz
+// FECHA DE CREACIÓN: Mayo 2026
+// DESCRIPCIÓN: Crear un programa que imprima: “Hola mundo!”.
+// =============================================================================
+//
+// 1 - Salida de resultados
 int main() {
     // Imprime el texto en la pantalla
     printf("Hola mundo en c++\n");
     
     return 0;
 }
+
+// 2 - Fin del programa
